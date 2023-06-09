782c6b307be9fa70c06428f705ffe819bd2ddb38
hakuryo1001 <hakuryo1001@protonmail.com>
2023-06-09 17:59:04 +0800

    logo

---
+++ links/index.js
@@ -0,0 +1,11 @@
+
+
+const links = {
+    twitter: "https://twitter.com/hakuryo1001",
+    github: "https://github.com/hakuryo1001",
+    telegram:"#"
+    // Add more links as needed
+  };
+  
+  export default links;
+  
